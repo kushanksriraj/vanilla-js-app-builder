c3fce564c3d04765fbfebe21ad2298c29816b578
Kushank Sriraj <kushanksriraj@gmail.com>
2021-06-06 16:47:01 +0530

    chors: babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,21 @@ const webpackConfig = {
     path: path.resolve(__dirname, "dist"),
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+
   mode: "production",
 };
 
