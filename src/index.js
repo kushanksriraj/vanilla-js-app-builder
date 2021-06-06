cca49372ca81673bac58e579e03c1a652c2a1c41
Kushank Sriraj <kushanksriraj@gmail.com>
2021-06-06 21:30:16 +0530

    chors: image webpack loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { App } from "./App";
+import LOGO from "./logo.svg"
 
 document.getElementById("root").innerHTML = `<h2> Hello </h2>
-${App("Kushank")}`;
+${App("Kushank")}<img src="${LOGO}" alt="build icon"/>`;
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,11 @@ const webpackConfig = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+
+      {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset"
+      }
     ],
   },
 
