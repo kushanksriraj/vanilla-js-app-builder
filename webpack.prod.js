a5b7161307717ab38a42c012ddee9c0d637d2b8d
Kushank Sriraj <kushanksriraj@gmail.com>
2021-06-08 02:04:21 +0530

    chors: added CSS extract plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,23 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const webpackConfig = merge(common, {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+      ignoreOrder: true,
+    }),
+  ],
   mode: "production",
 });
 
